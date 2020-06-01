5ac5bbeb16dfd69e79365b72b1107fce0c6df459
sanggeon <sssang97@svcorps.dev>
2020-06-01 16:56:49 +0900

    modified:   src/convert/number.ts

---
+++ src/convert/number.ts
@@ -0,0 +1,66 @@
+function decToBin(decimalString:string):string
+{
+    return "";
+}
+
+function decToOct(decimalString:string)
+{
+
+}
+
+function decToHex(decimalString:string)
+{
+
+}
+
+function binToDec(binString:string)
+{
+
+}
+
+function binToOct(binString:string)
+{
+
+}
+
+function binToHex(binString:string)
+{
+
+}
+
+function hexToDec(hexString:string)
+{
+
+}
+
+function hexToBin(hexString:string)
+{
+
+}
+
+function hexToOct(hexString:string)
+{
+
+}
+
+function octToDec(octString:string)
+{
+
+}
+
+function octToBin(octString:string)
+{
+
+}
+
+function octToHex(octString:string)
+{
+
+}
+
+module.exports = {
+    decToBin, decToOct, decToHex, 
+    binToDec, binToOct, binToHex,
+    hexToDec, hexToBin, hexToOct,
+    octToDec, octToBin, octToHex
+};
